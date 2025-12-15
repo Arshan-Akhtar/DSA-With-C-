8e6ecedbbddaac861f3b6ce629d9d9a96740de30
Arshan-Akhtar <arshanakhtar9100@gmail.com>
2025-12-16 01:50:21 +0530

    ignore exe files

--- Lecture7/array.cpp
+++ Lecture7/array.cpp
@@ -118,14 +118,39 @@ int main(){
 
 
    if(found){
-    cout<<"Yes";
+    cout<<"Yes"<<endl;
    }
    else
    {
-    cout<<"No";
+    cout<<"No"<<endl;
    }
 
+int arr[20];
+    
+    int size;
+    cout<<"Enter the size of array: "<<endl;
+    cin>>size;
 
+    cout<<"Enter the elements of array: "<<endl;
+    for(int i = 0; i < size; i++)
+    {
+        cin>>arr[i];
+    }
+
+    int heightest_marks = arr[0];
+    int index = 0;
+    for(int i = 0; i < size; i++)
+    {
+        if(arr[i] > heightest_marks)
+        {
+            heightest_marks = arr[i];
+            index = i;
+        }
+    }
+
+    cout<<heightest_marks<<" is heighest marks present at index: "<<index<<endl;
+
+    return 0;
 
 
 
--- README.md
+++
@@ -1,1 +0,0 @@
-DSA notes with C++ and Code
