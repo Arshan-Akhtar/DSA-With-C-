6e9fac1f29783ea9034a748b3a2596bebbbff126
Arshan-Akhtar <arshanakhtar9100@gmail.com>
2026-01-07 01:37:14 +0530

    13

--- Lecture13/insertionSort.cpp
+++ Lecture13/insertionSort.cpp
@@ -4,7 +4,6 @@ class Solution {
 public:
   vector<int> insertionSort(vector<int>& arr) {
     // Write your code here
-
 	int n = arr.size();
     
 	for(int i=1;i<n;i++){
@@ -16,13 +15,8 @@ public:
 			else{
 				break;
 			}
-
-
 			// code
-
-
 		}
-    
 	}
 
 	//  worst case
@@ -38,39 +32,7 @@ public:
 	// i =2, j= 1 operation
 	// i = 3, j= 1 operation
 	//  i = n-1, j = 1 operation
-
-
 	return arr;
-
-
-	
-
-
-
   }
 };
-// #include <bits/stdc++.h>
-// using namespace std;
 
-// class Solution {
-// public:
-//     vector<int> insertionSort(vector<int>& arr) {
-//         int n = arr.size();
-
-//         for (int i = 1; i < n; i++) {
-//             int key = arr[i];
-//             int j = i - 1;
-
-//             // Shift elements greater than key to one position ahead
-//             while (j >= 0 && arr[j] > key) {
-//                 arr[j + 1] = arr[j];
-//                 j--;
-//             }
-
-//             // Place key at its correct position
-//             arr[j + 1] = key;
-//         }
-
-//         return arr;
-//     }
-// };
